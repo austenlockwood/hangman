c06a6d114653ccbd70f2cdd36678d8489ddb0df2
Austen Lockwood <austenlockwood@gmail.com>
2016-09-07 13:00:29 -0400

    Different game response to valid and invalid guesses (such as guessing a number or symbol); cleaned-up README

--- hangman.rb
+++ hangman.rb
@@ -1,26 +1,8 @@
-require "pry"
-# TODO: handle invalid input (non-letters, already-guessed, more than one character)
-# TODO: show what letters already guessed
-# TODO: ask if they want to play
-# TODO: fix the initial display to show blanks
-
-# TODO: fix turns and don't display the answer at getgo
-
 words = ["HULLABALOO", "CLAPTRAP", "IDIOPATHIC", "BOBBIN", "BAMBOO", "POPPYCOCK", "PERSNICKETY", "IRKED", "NOODLES", "FRIPPERY", "BEFUDDLEMENT", "HABERDASHERY", "DIPHTHONG", "BRITCHES", "SASSAFRAS", "GADABOUTS", "BAZOOKA", "COCKAMAMIE", "EGAD", "MAGMA"]
 
 turns = 7
 secret_word = words.sample
 guesses = []
-
-def valid_guess(info, guesses)
-  if guesses.include?(info)
-    puts "You already guessed that.  Try again."
-  elsif (info.length == 1) && (info.to_i.to_s != info) && (info.to_f.to_s != info)
-    info = info
-  else
-    puts "'#{info}' is not a valid guess.  Try again."
-  end
-end
 
 def display(word, guesses)
   masked_word = ""
@@ -43,26 +25,34 @@ def win_lose(output, secret_word)
 end
 
 puts "We're playing hangman now.  The word is #{secret_word.length} letters long.  Guess a letter."
-# puts secret_word #get rid of this
+
 output = "-"
+
 until turns == 0 || !output.include?("-")
 
   stab = gets.chomp.upcase
 
-  worth_checking = valid_guess(stab, guesses)
-  if secret_word.include?(worth_checking)
-    binding.pry
-    puts "'#{worth_checking}' IS in the word!"
+  if guesses.include?(stab)
+    puts "You already guessed '#{stab}'.  Try again."
+  elsif (stab.length != 1) || (stab.to_i.to_s == stab) || (stab.to_f.to_s == stab)
+    puts "'#{stab}' is not a valid guess.  Try again."
   else
-    puts "'#{worth_checking}' is not in the word."
-    turns-=1
+
+    if secret_word.include?(stab)
+      puts "'#{stab}' IS in the word!"
+    else
+      puts "'#{stab}' is not in the word."
+      turns-=1
+    end
+
+    guesses << stab
+
   end
-  # win_lose_next(output, turns, secret_word)
-  guesses << worth_checking
-  output = display(secret_word, guesses)
-  puts output
-  puts "Already guessed: #{guesses.join(", ")}"
-  puts "Turns left: #{turns}"
+
+    output = display(secret_word, guesses)
+    puts output
+    puts "Already guessed: #{guesses.join(", ")}"
+    puts "Turns left: #{turns}"
 
 end
 
